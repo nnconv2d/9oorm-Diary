0bb64c320e031ef896a003d683b276c243f07361
nnconv2d <wwwept@naver.com>
2024-09-26 15:51:32 +0900

    Refactor: modify Login page

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import *
-from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = "users"
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', views.login, name='login'),
 ]
