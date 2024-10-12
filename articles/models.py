b050adac74f636360a62d1346511e87366cf543f
nnconv2d <wwwept@naver.com>
2024-10-12 14:09:38 +0900

    migrate: add articles' model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    contents = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
